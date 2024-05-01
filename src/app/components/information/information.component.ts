6890b2f77af133a44e8a17a528716f2b49316680
ArmandoxxXD <mamojose58@gmail.com>
2024-04-30 22:21:03 -0600

    Add: complete componente informacion

--- src/app/components/information/information.component.ts
+++ src/app/components/information/information.component.ts
@@ -1,16 +1,29 @@
 import { Component } from '@angular/core';
 import { PanelModule } from 'primeng/panel';
-
+import { ImageModule } from 'primeng/image';
+import { ChipModule } from 'primeng/chip';
+import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-information',
   standalone: true,
   imports: [
-    PanelModule
+    PanelModule,
+    ImageModule,
+    ChipModule,
+    CardModule,
+    DividerModule,
+    ButtonModule
   ],
   templateUrl: './information.component.html',
   styleUrl: './information.component.css'
 })
 export class InformationComponent {
+
+  constructor(){
+
+  }
 
 }
