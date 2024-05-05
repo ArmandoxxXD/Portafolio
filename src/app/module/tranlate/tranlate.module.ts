b98e9207bfda3600f53bf215b1c00dec86afd235
ArmandoxxXD <mamojose58@gmail.com>
2024-05-04 20:25:16 -0600

    Change: indexar texto español por default

--- src/app/module/tranlate/tranlate.module.ts
+++ src/app/module/tranlate/tranlate.module.ts
@@ -14,6 +14,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     CommonModule,
     HttpClientModule,
     TranslateModule.forRoot({
+      defaultLanguage:'es',
       loader: {
         provide: TranslateLoader,
         useFactory: HttpLoaderFactory,
