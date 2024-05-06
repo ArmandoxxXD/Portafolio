df9dcc5042c81fd43b1d64aca6c401a937eb9dab
ArmandoxxXD <mamojose58@gmail.com>
2024-05-06 15:07:02 -0600

    Change:  se agrego seccion 1 experiencia

--- src/app/components/sections/experience/experience.component.ts
+++ src/app/components/sections/experience/experience.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { PanelModule } from 'primeng/panel';
+import { TimelineModule } from 'primeng/timeline';
+import { TranlateModule } from '../../../module/tranlate/tranlate.module';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { ImageModule } from 'primeng/image';
+
+interface EventItem {
+  status?: string;
+  date?: string;
+  icon?: string;
+  color?: string;
+  image?: string;
+}
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [],
+  imports: [
+    PanelModule,
+    TranlateModule,
+    TimelineModule,
+    CardModule,
+    ButtonModule,
+    ImageModule
+  ],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.css'
 })
 export class ExperienceComponent {
+  events: EventItem[];
 
+  constructor() {
+      this.events = [
+          { status: 'Ordered', date: '15/10/2020 10:30', icon: 'pi pi-shopping-cart', color: '#9C27B0', image: 'game-controller.jpg' },
+          { status: 'Processing', date: '15/10/2020 14:00', icon: 'pi pi-cog', color: '#673AB7' },
+          { status: 'Shipped', date: '15/10/2020 16:15', icon: 'pi pi-shopping-cart', color: '#FF9800' }
+      ];
+  }
 }
